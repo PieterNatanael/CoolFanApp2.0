df1d9595571dbac0d8b662e8f78780bedc7a1976
PieterNatanael <pieternatanael@icloud.com>
2023-09-01 10:33:53 +0930

    Initial Commit

---
+++ CoolFanApp/CoolFanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolFanAppApp.swift
+//  CoolFanApp
+//
+//  Created by Pieter Yoshua Natanael on 01/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CoolFanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
